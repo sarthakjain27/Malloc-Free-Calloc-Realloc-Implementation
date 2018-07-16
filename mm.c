19a24a280a63d6047f70cd945862461b07381eae
sarthakjain27 <39550950+sarthakjain27@users.noreply.github.com>
2018-07-16 15:36:31 +0530

    Update mm.c

--- mm.c
+++ mm.c
@@ -93,7 +93,7 @@ static void* head_pointer(void *bp)
 
 static void* foot_pointer(void *bp)
 {
-    return ((char *)(bp) + GET_SIZE(head_pointer(bp)) - DSIZE);   
+    return ((char *)(bp) + gET_SIZE(head_pointer(bp)) - DSIZE);   
 }
 /* rounds up to the nearest multiple of ALIGNMENT */
 static size_t align(size_t x) {
@@ -105,7 +105,7 @@ static size_t GET(void *p)
     return (*(unsigned int *)(p));   
 }
 
-static size_t GET_SIZE(void *p)
+static size_t gET_SIZE(void *p)
 {
     return (GET(p) & ~0x7);   
 }
@@ -122,12 +122,12 @@ static size_t GET_PREV_ALLOC(void *p)
 
 static void *next_block_address(void *bp)
 {
-    return ((char *)(bp) + GET_SIZE(((char *)(bp) - WSIZE)));    
+    return ((char *)(bp) + gET_SIZE(((char *)(bp) - WSIZE)));    
 }
 
 static void *prev_block_address(void *bp)
 {
-    return ((char *)(bp) - GET_SIZE(((char *)(bp) - DSIZE)));   
+    return ((char *)(bp) - gET_SIZE(((char *)(bp) - DSIZE)));   
 }
 
 static size_t find_list(size_t asize)
