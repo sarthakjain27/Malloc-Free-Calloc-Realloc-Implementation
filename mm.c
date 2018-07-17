d130e11c1c926ca1aae5643c31cca98f81dae5be
sarthakjain27 <39550950+sarthakjain27@users.noreply.github.com>
2018-07-17 15:33:16 +0530

    Update mm.c

--- mm.c
+++ mm.c
@@ -54,18 +54,98 @@
 #define memcpy mem_memcpy
 #endif /* def DRIVER */
 
-/* What is the correct alignment? */
-#define ALIGNMENT 16
+/* Basic constants */
+typedef uint64_t word_t;
+static const size_t wsize = sizeof(word_t);   // word and header size (bytes)
+static const size_t dsize = 2*wsize;          // double word size (bytes)
+static const size_t min_block_size = 2*dsize; // Minimum block size
+static const size_t chunksize = (1 << 12);    // requires (chunksize % 16 == 0)
+
+static const word_t alloc_mask = 0x1;
+static const word_t size_mask = ~(word_t)0xF;
+
+typedef struct block
+{
+    /* Header contains size + allocation flag */
+    word_t header;
+    /*
+     * We don't know how big the payload will be.  Declaring it as an
+     * array of size 0 allows computing its starting address using
+     * pointer notation.
+     */
+    char payload[0];
+    /*
+     * We can't declare the footer as part of the struct, since its starting
+     * position is unknown
+     */
+} block_t;
+
+typedef struct free_block
+{
+    word_t header;
+    struct free_block *next_free;
+    struct free_block *prev_free;
+} block_f;
+
+static block_f *heap_start=NULL;
 
 /* rounds up to the nearest multiple of ALIGNMENT */
 static size_t align(size_t x) {
     return ALIGNMENT * ((x+ALIGNMENT-1)/ALIGNMENT);
 }
 
+bool mm_checkheap(int lineno);
+
+/* Function prototypes for internal helper routines */
+static block_t *extend_heap(size_t size);
+static void place(block_t *block, size_t asize);
+static block_t *find_fit(size_t asize);
+static block_t *coalesce(block_t *block);
+
+static size_t max(size_t x, size_t y);
+static size_t round_up(size_t size, size_t n);
+static word_t pack(size_t size, bool alloc);
+
+static size_t extract_size(word_t header);
+static size_t get_size(block_t *block);
+static size_t get_payload_size(block_t *block);
+
+static bool extract_alloc(word_t header);
+static bool get_alloc(block_t *block);
+
+static void write_header(block_t *block, size_t size, bool alloc);
+static void write_footer(block_t *block, size_t size, bool alloc);
+
+static block_t *payload_to_header(void *bp);
+static void *header_to_payload(block_t *block);
+
+static block_t *find_next(block_t *block);
+static word_t *find_prev_footer(block_t *block);
+static block_t *find_prev(block_t *block);
+
 /*
  * Initialize: return false on error, true on success.
  */
 bool mm_init(void) {
+    // Create the initial empty heap 
+    word_t *start = (word_t *)(mem_sbrk(2*wsize));
+
+    if (start == (void *)-1) 
+    {
+        return false;
+    }
+
+    start[0] = pack(0, true); // Prologue footer
+    start[1] = pack(0, true); // Epilogue header
+    // Heap starts with first "block header", currently the epilogue footer
+    heap_start = (block_f *) &(start[1]);
+    heap_start->next_free=NULL;
+    heap_start->prev_free=NULL;
+    // Extend the empty heap with a free block of chunksize bytes
+    if (extend_heap(chunksize) == NULL)
+    {
+        return false;
+    }
     return true;
 }
 
@@ -73,7 +153,46 @@ bool mm_init(void) {
  * malloc
  */
 void *malloc (size_t size) {
-    return NULL;
+   dbg_requires(mm_checkheap(__LINE__));
+    size_t asize;      // Adjusted block size
+    size_t extendsize; // Amount to extend heap if no fit is found
+    block_t *block;
+    void *bp = NULL;
+
+    if (heap_start == NULL) // Initialize heap if it isn't initialized
+    {
+        mm_init();
+    }
+
+    if (size == 0) // Ignore spurious request
+    {
+        dbg_ensures(mm_checkheap(__LINE__));
+        return bp;
+    }
+
+    // Adjust block size to include overhead and to meet alignment requirements
+    asize = round_up(size + dsize, dsize);
+
+    // Search the free list for a fit
+    block = find_fit(asize);
+
+    // If no fit is found, request more memory, and then and place the block
+    if (block == NULL)
+    {  
+        extendsize = max(asize, chunksize);
+        block = extend_heap(extendsize);
+        if (block == NULL) // extend_heap returns an error
+        {
+            return bp;
+        }
+
+    }
+
+    place(block, asize);
+    bp = header_to_payload(block);
+
+    dbg_ensures(mm_checkheap(__LINE__));
+    return bp;
 }
 
 /*
@@ -134,9 +253,291 @@ static bool aligned(const void *p) {
  *****************************************************************************
  */
 
+static void place(block_t *block, size_t asize)
+{
+    size_t csize = get_size(block);
+
+    if ((csize - asize) >= min_block_size)
+    {
+        block_t *block_next;
+        write_header(block, asize, true);
+        write_footer(block, asize, true);
+
+        block_next = find_next(block);
+        write_header(block_next, csize-asize, false);
+        write_footer(block_next, csize-asize, false);
+        block=(block_t *)block;
+        block_next=(block_f *)block_next;
+        
+        block_next->next_free=block->next_free;
+        block->prev_free->next_free=block_next;
+        block_next->prev_free=block->prev_free;
+        block_next->next_free->prev_free=block_next;
+    }
+
+    else
+    { 
+        write_header(block, csize, true);
+        write_footer(block, csize, true);
+    }
+}
+
 /*
  * mm_checkheap
  */
 bool mm_checkheap(int lineno) {
     return true;
 }
+
+static block_t *extend_heap(size_t size) 
+{
+    void *bp;
+
+    // Allocate an even number of words to maintain alignment
+    size = round_up(size, dsize);
+    if ((bp = mem_sbrk(size)) == (void *)-1)
+    {
+        return NULL;
+    }
+    
+    // Initialize free block header/footer 
+    block_t *block = payload_to_header(bp);
+    write_header(block, size, false);
+    write_footer(block, size, false);
+    // Create new epilogue header
+    block_t *block_next = find_next(block);
+    write_header(block_next, 0, true);
+
+    // Coalesce in case the previous block was free
+    return coalesce(block);
+}
+
+/*
+ * <what does coalesce do?>
+ */
+static block_t *coalesce(block_t * block) 
+{
+    block_t *block_next = find_next(block);
+    block_t *block_prev = find_prev(block);
+
+    bool prev_alloc = extract_alloc(*(find_prev_footer(block)));
+    bool next_alloc = get_alloc(block_next);
+    size_t size = get_size(block);
+
+    if (prev_alloc && next_alloc)              // Case 1
+    {
+        return block;
+    }
+
+    else if (prev_alloc && !next_alloc)        // Case 2
+    {
+        size += get_size(block_next);
+        write_header(block, size, false);
+        write_footer(block, size, false);
+        block=(block_f *)block;
+        block_next=(block_f *) block_next;
+        block->next_free=heap_start;
+        heap_start->prev_free=block;
+        block->prev_free=NULL;
+        heap_start=block;
+        block_next->prev_free->next_free=block_next->next_free;
+        block_next->next_free->prev_free=block_next->prev_free;
+    }
+
+    else if (!prev_alloc && next_alloc)        // Case 3
+    {
+        size += get_size(block_prev);
+        write_header(block_prev, size, false);
+        write_footer(block_prev, size, false);
+        block_prev=(block_f *)block_prev;
+        block_prev->prev_free->next_free=block_prev->next_free;
+        block_prev->next_free->prev_free=block_prev->prev_free;
+        block_prev->next_free=heap_start;
+        heap_start->prev_free=block_prev;
+        heap_start=block_prev;
+        block_prev->prev_free=NULL;
+        block=block_prev;
+    }
+
+    else                                        // Case 4
+    {
+        size += get_size(block_next) + get_size(block_prev);
+        write_header(block_prev, size, false);
+        write_footer(block_prev, size, false);
+        block_prev=(block_f *)block_prev;
+        block_next=(block_f *)block_next;
+        block_prev->prev_free->next_free=block_prev->next_free;
+        block_prev->next_free->prev_free=block_prev->prev_free;
+        block_prev->next_free=heap_start;
+        heap_start->prev_free=block_prev;
+        heap_start=block_prev;
+        block_prev->prev_free=NULL;
+        block_next->prev_free->next_free=block_next->next_free;
+        block_next->next_free->prev_free=block_next->prev_free;
+        block=block_prev;
+    }
+    return (block_t *)block;
+}
+
+static block_t *find_fit(size_t asize)
+{
+    block_f *block;
+
+    for (block = heap_start; get_size((block_t *)block) > 0;
+                             block = block->next_free)
+    {
+        if (!(get_alloc((block_t *)block)) && (asize <= get_size((block_t *)block)))
+        {
+            return (block_t *)block;
+        }
+    }
+    return NULL; // no fit found
+}
+
+
+/*
+ * max: returns x if x > y, and y otherwise.
+ */
+static size_t max(size_t x, size_t y)
+{
+    return (x > y) ? x : y;
+}
+
+/*
+ * round_up: Rounds size up to next multiple of n
+ */
+static size_t round_up(size_t size, size_t n)
+{
+    return (n * ((size + (n-1)) / n));
+}
+
+/*
+ * pack: returns a header reflecting a specified size and its alloc status.
+ *       If the block is allocated, the lowest bit is set to 1, and 0 otherwise.
+ */
+static word_t pack(size_t size, bool alloc)
+{
+    return alloc ? (size | alloc_mask) : size;
+}
+
+
+/*
+ * extract_size: returns the size of a given header value based on the header
+ *               specification above.
+ */
+static size_t extract_size(word_t word)
+{
+    return (word & size_mask);
+}
+
+/*
+ * get_size: returns the size of a given block by clearing the lowest 4 bits
+ *           (as the heap is 16-byte aligned).
+ */
+static size_t get_size(block_t *block)
+{
+    return extract_size(block->header);
+}
+
+/*
+ * get_payload_size: returns the payload size of a given block, equal to
+ *                   the entire block size minus the header and footer sizes.
+ */
+static word_t get_payload_size(block_t *block)
+{
+    size_t asize = get_size(block);
+    return asize - dsize;
+}
+
+/*
+ * extract_alloc: returns the allocation status of a given header value based
+ *                on the header specification above.
+ */
+static bool extract_alloc(word_t word)
+{
+    return (bool)(word & alloc_mask);
+}
+
+/*
+ * get_alloc: returns true when the block is allocated based on the
+ *            block header's lowest bit, and false otherwise.
+ */
+static bool get_alloc(block_t *block)
+{
+    return extract_alloc(block->header);
+}
+
+/*
+ * write_header: given a block and its size and allocation status,
+ *               writes an appropriate value to the block header.
+ */
+static void write_header(block_t *block, size_t size, bool alloc)
+{
+    block->header = pack(size, alloc);
+}
+
+
+/*
+ * write_footer: given a block and its size and allocation status,
+ *               writes an appropriate value to the block footer by first
+ *               computing the position of the footer.
+ */
+static void write_footer(block_t *block, size_t size, bool alloc)
+{
+    word_t *footerp = (word_t *)((block->payload) + get_size(block) - dsize);
+    *footerp = pack(size, alloc);
+}
+
+
+/*
+ * find_next: returns the next consecutive block on the heap by adding the
+ *            size of the block.
+ */
+static block_t *find_next(block_t *block)
+{
+    dbg_requires(block != NULL);
+    block_t *block_next = (block_t *)(((char *)block) + get_size(block));
+
+    dbg_ensures(block_next != NULL);
+    return block_next;
+}
+
+/*
+ * find_prev_footer: returns the footer of the previous block.
+ */
+static word_t *find_prev_footer(block_t *block)
+{
+    // Compute previous footer position as one word before the header
+    return (&(block->header)) - 1;
+}
+
+/*
+ * find_prev: returns the previous block position by checking the previous
+ *            block's footer and calculating the start of the previous block
+ *            based on its size.
+ */
+static block_t *find_prev(block_t *block)
+{
+    word_t *footerp = find_prev_footer(block);
+    size_t size = extract_size(*footerp);
+    return (block_t *)((char *)block - size);
+}
+
+/*
+ * payload_to_header: given a payload pointer, returns a pointer to the
+ *                    corresponding block.
+ */
+static block_t *payload_to_header(void *bp)
+{
+    return (block_t *)(((char *)bp) - offsetof(block_t, payload));
+}
+
+/*
+ * header_to_payload: given a block pointer, returns a pointer to the
+ *                    corresponding payload.
+ */
+static void *header_to_payload(block_t *block)
+{
+    return (void *)(block->payload);
+}
+
