08af4ee785667e80314327dbcc4fbe64f700b190
sarthakjain27 <39550950+sarthakjain27@users.noreply.github.com>
2018-07-17 12:50:18 +0530

    Update mm.c

--- mm.c
+++ mm.c
@@ -55,31 +55,256 @@
 #endif /* def DRIVER */
 
 /* What is the correct alignment? */
-#define ALIGNMENT 16
+/* Basic constants */
+typedef uint64_t word_t;
+static const size_t wsize = sizeof(word_t);   // word and header size (bytes)
+static const size_t dsize = 2*wsize;          // double word size (bytes)
+static const size_t min_block_size = 2*dsize; // Minimum block size
+static const size_t chunksize = (1 << 12);    // requires (chunksize % 16 == 0)
+
+static const word_t alloc_mask = 0x1;
+static const word_t size_mask = ~(word_t)0xF;
+
+typedef struct block hblock;
+
+struct block {
+    size_t header;
+    hblock *succ_p;
+    hblock *pred_p;
+    size_t footer;
+};
+
+#define ALIGNMENT 2*dsize;
+
+static size_t ALIGN(size_t x) {
+    return ((x + (ALIGNMENT - 1)) & ~0XF);
+}
+
+#define HSIZE      ALIGN(sizeof(hblock)) /* The minimum size of a block */
+
+*
+ * max: returns x if x > y, and y otherwise.
+ */
+static size_t max(size_t x, size_t y)
+{
+    return (x > y) ? x : y;
+}
+
+/*
+ * round_up: Rounds size up to next multiple of n
+ */
+static size_t round_up(size_t size, size_t n)
+{
+    return (n * ((size + (n-1)) / n));
+}
+
+/*
+ * pack: returns a header reflecting a specified size and its alloc status.
+ *       If the block is allocated, the lowest bit is set to 1, and 0 otherwise.
+ */
+static word_t pack(size_t size, bool alloc)
+{
+    return alloc ? (size | alloc_mask) : size;
+}
+
+/*
+ * get_size: returns the size of a given block by clearing the lowest 4 bits
+ *           (as the heap is 16-byte aligned).
+ */
+static size_t get_size(hblock *block)
+{
+    return extract_size(block->header);
+}
+
+/*
+ * get_alloc: returns true when the block is allocated based on the
+ *            block header's lowest bit, and false otherwise.
+ */
+static bool get_alloc(hblock *block)
+{
+    return extract_alloc(block->header);
+}
 
 /* rounds up to the nearest multiple of ALIGNMENT */
 static size_t align(size_t x) {
     return ALIGNMENT * ((x+ALIGNMENT-1)/ALIGNMENT);
 }
 
+/* Private global variables */
+static hblock *p;         /* Points to the starting block at all times */
+
 /*
  * Initialize: return false on error, true on success.
  */
 bool mm_init(void) {
+    /* Attempt to create an empty heap with just a prologue 
+     * at the beginning 
+     */
+    if (mem_sbrk(HSIZE) == (void *)-1)
+        return false;
+ 
+    p = (hblock *) mem_heap_lo();
+
+    /* Set the block size of the header, and make it point to itself */
+    p->header = HSIZE | 0x1; // The prologue will be the only allocated block of HSIZE
+    p->footer = p->header;
+    p->succ_p = p;
+    p->pred_p = p;
+
     return true;
+}
+
+/*
+ * Attempts to find the right size of free block in the free list. The free list is 
+ * implemented as an explicit list, which is simply a doubly linked list.
+ */
+static void *find_free(size_t size)
+{
+    /* Iterate over each of the blocks in the free list until a block
+     * of the appropriate size is found. If the block wraps back around
+     * to the prologue, then a free block wasn't found.
+     */ 
+    hblock *bp;
+    for(bp = p->succ_p; bp != p && bp->header < size; bp = bp->succ_p) {
+    }
+
+    /* If the free list wrapped back around, then there were no free spots */
+    if (bp == p)
+        return NULL;
+    else
+    /* Otherwise return the pointer to the free block */
+        return bp; 
+}
+
+static void remove_free_block(hblock *bp)
+{
+    bp->pred_p->succ_p = bp->succ_p;
+    bp->succ_p->pred_p = bp->pred_p;
+}
+
+static void *coalesce(hblock *bp)
+{
+    size_t prev_alloc = GET_ALLOC(FTRP(PREV_BLKP(bp)));
+    size_t next_alloc = GET_ALLOC(HDRP(NEXT_BLKP(bp)));
+    size_t size = GET_SIZE(bp);
+
+    /* If the next block is free, then coalesce the current
+     * block (bp) and the next block */
+    if (prev_alloc && !next_alloc) {           /* Case 2 */
+        size += GET_SIZE(HDRP(NEXT_BLKP(bp)));
+        bp->header = size | 0x0;
+        bp->footer = bp->header;
+    }
+
+    /* If the previous block is free, then coalesce the current
+     * block (bp) and the previous block */
+    else if (!prev_alloc && next_alloc) {      /* Case 3 */
+        size += GET_SIZE(FTRP(PREV_BLKP(bp)));
+        bp->footer = size | 0x0;
+        bp->pred_p->header = size | 0x0;
+    }
+
+    
+    else if (!prev_alloc && !next_alloc) {
+        size += GET_SIZE(FTRP(PREV_BLKP(bp))) + 
+                GET_SIZE(HDRP(NEXT_BLKP(bp)));
+        bp->pred_p->header = size | 0x0;
+        bp->succ_p->footer = size | 0x0;
+    }
+
+    return bp;
+}
+
+static void place(hblock *bp, size_t newsize)
+{  
+    size_t csize = GET_SIZE(bp->header);
+
+    if ((csize - newsize) >= HSIZE) {
+        bp->header = newsize | 0x1;
+        bp->footer = bp->header;
+        remove_free_block(bp);
+        bp = (hblock *) NEXT_BLKP(bp);
+        bp->header = (csize-newsize) | 0x0;
+        bp->footer = bp->header; 
+        coalesce(bp);
+    }
+
+
+    else {
+        bp->header = csize | 0x1;
+        bp->footer = bp->header;
+        remove_free_block(bp);
+    }
+    /* Set the allocated bit of the header and footer */
+    //bp->header |= 0x1;
+    //bp->footer = bp->header;
+
+    /* Set up the link for the free list */  
+    //remove_free_block(bp);
+
+    return;
 }
 
 /*
  * malloc
  */
 void *malloc (size_t size) {
-    return NULL;
+    /* Ignore spurious requests */
+    if (size < 1)
+        return NULL;
+
+    /* The size of the new block is equal to the size of the header, plus
+     * the size of the payload
+     */
+    int newsize = ALIGN(size + HSIZE);
+    
+    /* Try to find a free block that is large enough */
+    hblock *bp = (hblock *) find_free(newsize);
+   
+    /* If a large enough free block was not found, then coalesce
+     * the existing free blocks */ 
+
+    /* After coalsecing, if a large enough free block cannot be found, then
+     * extend the heap with a free block */
+    if (bp == NULL) { 
+        bp = mem_sbrk(newsize);
+        if ((long)(bp) == -1)
+            return NULL;
+        else {
+            bp->header = newsize | 0x1;
+            bp->footer = bp->header;
+        }
+    }
+    else {
+        /* Otherwise, a free block of the appropriate size was found. Place
+         * the block */
+        place(bp, newsize); 
+    }
+
+    // Return a pointer to the payload
+    return (char *) bp + HSIZE;
 }
 
 /*
  * free
  */
 void free (void *ptr) {
+    /* Get the pointer to the allocated block */
+    hblock *bp = ptr - HSIZE; 
+    //hblock *cbp; // stores a pointer to the coalesced block
+
+    //cbp = (hblock *) coalesce(bp);
+
+    /* Modify the allocated bit for the header and footer */ 
+    bp->header &= ~1;
+    bp->footer = bp->header;
+
+    /* Set up the doubly linked explicit free list */
+    bp->succ_p = p->succ_p;
+    bp->pred_p = p;
+    p->succ_p = bp;
+    bp->succ_p->pred_p = bp;
+   
     return;
 }
 
@@ -95,7 +320,21 @@ void *realloc(void *oldptr, size_t size) {
  * This function is not tested by mdriver
  */
 void *calloc (size_t nmemb, size_t size) {
+    void *bp;
+    size_t asize = elements * size;
+
+    if (asize/elements != size)
+    // Multiplication overflowed
     return NULL;
+    
+    bp = malloc(asize);
+    if (bp == NULL)
+    {
+        return NULL;
+    }
+    // Initialize all bits to 0
+    memset(bp, 0, asize);
+    return bp;
 }
 
 
